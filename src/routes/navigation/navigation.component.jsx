3db791b05b647ad02280d05bc611db330b21f30d
holycrys <cholingher@gmail.com>
2024-01-09 12:21:45 +0200

    added products-context, cart-icon and cart-dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,6 +7,8 @@ import {signOutUser} from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation =() => {
   const {currentUser} = useContext(UserContext);
@@ -30,8 +32,10 @@ const Navigation =() => {
             SIGN IN
             </Link>
            )    
-          }   
+          }  
+          <CartIcon/> 
         </div>
+        <CartDropdown/>
       </div>
       <Outlet />
     </Fragment>
